46debae61e13c05eea228bf8b6e733cda3be684c
jjm <1632384718@qq.com>
2017-05-17 13:39:41 +0800

    第一次提交2017年5月17日13:39:38

---
+++ src/com/caicainiao/Hello.java
@@ -0,0 +1,11 @@
+package com.caicainiao;
+
+/**
+ * Created by Administrator on 2017/5/17.
+ * 2017/5/17 13:38
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("2017Äê5ÔÂ17ÈÕ13:38:44");
+    }
+}
