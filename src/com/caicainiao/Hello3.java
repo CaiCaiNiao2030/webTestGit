6b57ba997465307b0fc9bb80265b1d0b777f0be1
jjm <1632384718@qq.com>
2017-05-17 14:40:33 +0800

    2017年5月17日14:35:17提交

--- src/com/caicainiao/Hello3.java
+++ src/com/caicainiao/Hello3.java
@@ -1,1 +1,11 @@
-这是Hello3
+package com.caicainiao;
+
+/**
+ * Created by Administrator on 2017/5/17.
+ * 2017/5/17 14:31
+ */
+public class Hello3 {
+    public static void main(String[] args) {
+        System.out.println("kkkkk");
+    }
+}
