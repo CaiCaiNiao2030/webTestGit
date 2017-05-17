e3c7cf8c34fc6f0579d1cfa9cef3ec96a91a75b9
jjm <1632384718@qq.com>
2017-05-17 14:33:40 +0800

    2017年5月17日14:33:37提交

--- src/com/caicainiao/Hello.java
+++ src/com/caicainiao/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("2017年5月17日13:38:44");
         System.out.println("修改2017年5月17日13:42:00");
+        System.out.println("2017年5月17日14:33:25");
     }
 }
