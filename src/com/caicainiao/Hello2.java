f794fd086b59aca0bcb3a49a989b6df5d1c52b23
jjm <1632384718@qq.com>
2017-05-17 14:35:21 +0800

    2017年5月17日14:35:17提交

---
+++ src/com/caicainiao/Hello2.java
@@ -0,0 +1,11 @@
+package com.caicainiao;
+
+/**
+ * Created by Administrator on 2017/5/17.
+ * 2017/5/17 14:31
+ */
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("kkkkk");
+    }
+}
